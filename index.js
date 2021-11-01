c7f9644ce7ba7fe0adc1817db3889274f3f6ceb1
Guy L <g.guy.lab@gmail.com>
2021-11-01 19:06:59 -0400

    sup world

---
+++ index.js
@@ -0,0 +1,5 @@
+$(function() {
+    $("body").css("background-image", "url(backgrounds/" + (Math.floor(Math.random() * 38)+1) + ".png)");
+	$("body").css("background-repeat", "no-repeat");
+	$("body").css("background-size", "100% auto");
+});
