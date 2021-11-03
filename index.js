d044283de7ea44eec4d829c182c1af4fd500a895
Guy L <g.guy.lab@gmail.com>
2021-11-02 21:52:40 -0400

    db testing

--- index.js
+++ index.js
@@ -2,4 +2,14 @@ $(function() {
     $("body").css("background-image", "url(backgrounds/" + (Math.floor(Math.random() * 38)+1) + ".png)");
 	$("body").css("background-repeat", "no-repeat");
 	$("body").css("background-size", "100% auto");
+	
+	const db = getDatabase();
+    //const contents = db.exec("SELECT * FROM my_table");
 });
+
+async function getDatabase(){
+	const SQL = await initSqlJs();
+    const fetched = await fetch("truescore.db");
+    const buf = await fetched.arrayBuffer();
+    return new SQL.Database(new Uint8Array(buf));
+}
