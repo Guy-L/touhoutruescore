03d28a1c4029c0edcfaa3c95d30e2350372e315b
Guy L <g.guy.lab@gmail.com>
2021-11-02 23:50:19 -0400

    stuff

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
-var db;
-
 $(function() {
     $("body").css("background-image", "url(backgrounds/" + (Math.floor(Math.random() * 38)+1) + ".png)");
 	$("body").css("background-repeat", "no-repeat");
 	$("body").css("background-size", "100% auto");
+	var umboard = document.getElementById("umboard");
 	
-	alert("This is a WIP page. Please come by later.");
-	db = getDatabase();
-	//alert(db);
-    //const contents = db.exec("SELECT * FROM my_table");
+	alert("This is a WIP site. Please come back later.");
+	getUMData().then((um) => {
+		for(let replay of um[0].values){
+			let row = umboard.insertRow();
+			
+			for(let data in replay){
+				let cell = row.insertCell(-1);
+				cell.innerHTML = replay[data];
+			}
+		};
+	});
 });
 
 async function getDatabase(){
 	const SQL = await initSqlJs();
     const fetched = await fetch("truescore.db");
     const buf = await fetched.arrayBuffer();
-	const data = new SQL.Database(new Uint8Array(buf));
-	const test = data.exec("SELECT * FROM wbawc");
-    return data;
+	const db = new SQL.Database(new Uint8Array(buf));
+    return db;
+}
+
+async function getUMData(){
+	db = await getDatabase();
+	return db.exec("SELECT * from um;");
 }
