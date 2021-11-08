82c528b2095e616fd576a810ef823e3595508d00
Guy L <g.guy.lab@gmail.com>
2021-11-07 19:29:58 -0500

    style updates

--- index.js
+++ index.js
@@ -5,15 +5,22 @@ $(function() {
 	var umboard = document.getElementById("umboard");
 	
 	alert("This is a WIP site. Please come back later.");
+	
 	getUMData().then((um) => {
 		for(let replay of um[0].values){
 			let row = umboard.insertRow();
 			
-			for(let data in replay){
+			for(let data = 0; data < replay.length - 1; data++){
 				let cell = row.insertCell(-1);
-				cell.innerHTML = replay[data];
+				
+				if(data == 7) cell.innerHTML = replay[data];
+				else if(data == 12) cell.innerHTML = "<a href=\"" + replay[data] + "\">Resource</a>" + (replay[29] == 1 ? "*":"");
+				else if(data > 20 && data < 29) cell.innerHTML = "<img src=\"cards/" + replay[data] + ".png\" alt=\"" + replay[data] + "\" title=\"" + replay[data] + "\">";
+				else cell.innerHTML = isNaN(replay[data]) ? replay[data] : Number(replay[data]).toLocaleString();
 			}
-		};
+		}
+		
+		sorttable.makeSortable(umboard);
 	});
 });
 
@@ -27,5 +34,5 @@ async function getDatabase(){
 
 async function getUMData(){
 	db = await getDatabase();
-	return db.exec("SELECT * from um;");
+	return db.exec("SELECT * from um ORDER BY TrueScore DESC;");
 }
