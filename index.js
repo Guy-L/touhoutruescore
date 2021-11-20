e3122b530f23b433beee2cdf20f82c3708ab22e0
Guy L <g.guy.lab@gmail.com>
2021-11-20 03:41:55 -0500

    .

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ $(function() {
     $("body").css("background-image", "url(backgrounds/" + (Math.floor(Math.random() * 38)+1) + ".png)");
 	$("body").css("background-repeat", "no-repeat");
 	$("body").css("background-size", "100% auto");
-	var umtable = document.getElementById("umtable");
 		
 	getUMData().then((um) => {
-		for(let replay of um[0].values){
+		//Initialization
+		var umtable = document.getElementById("umtable");
+		var umnormal = document.getElementById("umnormal");
+		var umbig = document.getElementById("umbig");
+		
+		//Generation
+		for(let [i, replay] of um[0].values.entries()){
+			
+			//Table View
 			let row = umtable.insertRow();
 			
 			for(let data = 0; data < replay.length - 1; data++){
@@ -19,15 +26,47 @@ $(function() {
 				else if(data > 19 && data < 28) cell.innerHTML = "<img src=\"cards/" + replay[data] + ".png\" alt=\"" + replay[data] + "\" title=\"" + replay[data] + "\" class=\"card\">";
 				else cell.innerHTML = isNaN(replay[data]) ? replay[data] : Number(replay[data]).toLocaleString();
 			}
+			
+			//Normal View
+			let cls = (i==0)? "class=\"tip-under\"":"";
+			let lives = "<img src=\"resources/life.png\" alt=\"L\">".repeat(replay[16]) + (replay[17] > 0 || replay[16] == 0 ? "<img src=\"resources/lifepiece"+replay[17]+".png\" alt=\"+"+replay[17]+"\">" : "");
+			let bombs = "<img src=\"resources/bomb.png\" alt=\"B\">".repeat(replay[18]) + (replay[19] > 0 || replay[18] == 0 ? "<img src=\"resources/bombpiece"+replay[19]+".png\" alt=\"+"+replay[19]+"\">" : "");
+			
+			
+			$(umnormal).append(
+			`
+			<div class="entry-normal">
+				<div class="entry-normal-top" onclick="expandNormal(this)">
+					<table><tr>
+					<td>#${i+1}</td>
+					<td style="width:46%; padding-left: 10px;">${replay[3]} <span class="subName">${replay[2]==replay[3]?"":"("+replay[2]+")"}</span></td>
+					<td style="width:12%; text-align: center; text-shadow: 0px 0px 5px black, 0px 0px 5px black, 0px 0px 5px black;">${replay[4].toLocaleString()}</td>
+					<td style="width:5%; text-align: right;">${replay[15].toLocaleString()} <img src="gold.png" alt="G"></td>
+					<td style="width:10.5%; text-align: center;" ${cls} data-tip="Replay date (${replay[8]})
+Submitted: ${replay[12]}">${replay[7]}</td>
+					<td style="width:4%; text-align: center;" ${cls} data-tip="Slowdown %">${replay[5].toFixed(2)}%</td>
+					<td style="width:6%; text-align: center;" ${cls} data-tip="Ending lives">${lives}</td>
+					<td style="width:6%; text-align: center;" ${cls} data-tip="Ending bombs">${bombs}</td>
+					<td style="width:3%; text-align: center;" ${cls} data-tip="Click to download replay"><a href="${replay[10]}" onclick="stopEvent(event);"><img src="royalflare.png" alt="${replay[11]}"></a></td>
+					<td style="width:7.5%; text-align: center;"><div class="moreInfo">More Info</div></td>
+					</tr></table>
+				</div>
+				<div class="entry-normal-bottom">
+					${replay[9].replace("//", "\n")}
+				</div>
+			</div>
+			`);
+			
 		}
 		
 		sorttable.makeSortable(umtable);
 		
+		//Applying Stored Selectors / View
 		updateDiff($(".select-selected")[0].innerHTML);
 		updateChar($(".select-selected")[1].innerHTML);
 		updateSort($(".select-selected")[2].innerHTML);
 		if (localStorage['viewMode']) changeView(localStorage['viewMode']);
-		else changeView(2);
+		else changeView(1);
 	});
 });
 
@@ -110,6 +149,16 @@ function changeView(mode, o){
 	
 	$(".visible-view").removeClass("visible-view");
 	$("#umboard").children()[mode].classList.add("visible-view");
+}
+
+function expandNormal(entry){
+	$(entry).siblings('.entry-normal-bottom').slideToggle(300);  
+	moreInfo = $(entry).find('.moreInfo');
+	moreInfo.html(moreInfo.html() == "More Info"?"Less Info":"More Info");
+}
+
+function stopEvent(e) {
+	e.stopPropagation();
 }
 
 //TODO: 
